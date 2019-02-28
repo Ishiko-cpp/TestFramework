b199ea9355197a36c16bcdb6b261511ddd5932c8
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2019-02-28 23:05:36 +0000

    Updates

--- Core/Source/TopTestSequence.cpp
+++ Core/Source/TopTestSequence.cpp
@@ -1,23 +1,23 @@
 /*
-	Copyright (c) 2015 Xavier Leclercq
+    Copyright (c) 2015-2019 Xavier Leclercq
 
-	Permission is hereby granted, free of charge, to any person obtaining a
-	copy of this software and associated documentation files (the "Software"),
-	to deal in the Software without restriction, including without limitation
-	the rights to use, copy, modify, merge, publish, distribute, sublicense,
-	and/or sell copies of the Software, and to permit persons to whom the
-	Software is furnished to do so, subject to the following conditions:
+    Permission is hereby granted, free of charge, to any person obtaining a
+    copy of this software and associated documentation files (the "Software"),
+    to deal in the Software without restriction, including without limitation
+    the rights to use, copy, modify, merge, publish, distribute, sublicense,
+    and/or sell copies of the Software, and to permit persons to whom the
+    Software is furnished to do so, subject to the following conditions:
 
-	The above copyright notice and this permission notice shall be included in
-	all copies or substantial portions of the Software.
+    The above copyright notice and this permission notice shall be included in
+    all copies or substantial portions of the Software.
 
-	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
-	THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
-	FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
-	IN THE SOFTWARE.
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+    THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+    FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+    IN THE SOFTWARE.
 */
 
 #include "TopTestSequence.h"
@@ -28,13 +28,13 @@ namespace TestFramework
 {
 
 TopTestSequence::TopTestSequence(const std::string& name)
-	: TestSequence(TestNumber(), name)
+    : TestSequence(TestNumber(), name)
 {
 }
 
 TopTestSequence::TopTestSequence(const std::string& name,
-	const TestEnvironment& environment)
-	: TestSequence(TestNumber(), name, environment)
+    const TestEnvironment& environment)
+    : TestSequence(TestNumber(), name, environment)
 {
 }
 
@@ -43,11 +43,11 @@ TopTestSequence::~TopTestSequence()
 }
 
 void TopTestSequence::notify(TestObserver::EEventType type,
-							 TestObserver::ptr& observer)
+                             TestObserver::ptr& observer)
 {
-	// Do nothing because the top level sequence is a 
-	// sequence hidden to the user and used by the test 
-	// harness internally only
+    // Do nothing because the top level sequence is a 
+    // sequence hidden to the user and used by the test 
+    // harness internally only
 }
 
 }
