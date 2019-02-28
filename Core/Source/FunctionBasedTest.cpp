265c25da21a900ec5f81aaae0b475d9b01650f3c
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2019-02-28 23:10:09 +0000

    Updates

--- Core/Source/FunctionBasedTest.cpp
+++ Core/Source/FunctionBasedTest.cpp
@@ -1,23 +1,23 @@
 /*
-	Copyright (c) 2000-2015 Xavier Leclercq
+    Copyright (c) 2000-2019 Xavier Leclercq
 
-	Permission is hereby granted, free of charge, to any person obtaining a
-	copy of this software and associated documentation files (the "Software"),
-	to deal in the Software without restriction, including without limitation
-	the rights to use, copy, modify, merge, publish, distribute, sublicense,
-	and/or sell copies of the Software, and to permit persons to whom the
-	Software is furnished to do so, subject to the following conditions:
+    Permission is hereby granted, free of charge, to any person obtaining a
+    copy of this software and associated documentation files (the "Software"),
+    to deal in the Software without restriction, including without limitation
+    the rights to use, copy, modify, merge, publish, distribute, sublicense,
+    and/or sell copies of the Software, and to permit persons to whom the
+    Software is furnished to do so, subject to the following conditions:
 
-	The above copyright notice and this permission notice shall be included in
-	all copies or substantial portions of the Software.
+    The above copyright notice and this permission notice shall be included in
+    all copies or substantial portions of the Software.
 
-	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
-	THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
-	FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
-	IN THE SOFTWARE.
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+    THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+    FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+    IN THE SOFTWARE.
 */
 
 #include "FunctionBasedTest.h"
@@ -28,32 +28,32 @@ namespace TestFramework
 {
 
 FunctionBasedTest::FunctionBasedTest(const TestNumber& number, 
-	                                 const std::string& name,
-								     TestResult::EOutcome (*runFct)())
-	: Test(number, name), m_runFctVoid(runFct), m_runFctTest(0)
+                                     const std::string& name,
+                                     TestResult::EOutcome (*runFct)())
+    : Test(number, name), m_runFctVoid(runFct), m_runFctTest(0)
 {
 }
 
 FunctionBasedTest::FunctionBasedTest(const TestNumber& number,
-									 const std::string& name,
-									 TestResult::EOutcome (*runFct)(),
-									 const TestEnvironment& environment)
-	: Test(number, name, environment), m_runFctVoid(runFct), m_runFctTest(0)
+                                     const std::string& name,
+                                     TestResult::EOutcome (*runFct)(),
+                                     const TestEnvironment& environment)
+    : Test(number, name, environment), m_runFctVoid(runFct), m_runFctTest(0)
 {
 }
 
 FunctionBasedTest::FunctionBasedTest(const TestNumber& number,
-		                             const std::string& name,
-									 TestResult::EOutcome(*runFct)(Test& test))
-	: Test(number, name), m_runFctVoid(0), m_runFctTest(runFct)
+                                     const std::string& name,
+                                     TestResult::EOutcome(*runFct)(Test& test))
+    : Test(number, name), m_runFctVoid(0), m_runFctTest(runFct)
 {
 }
 
 FunctionBasedTest::FunctionBasedTest(const TestNumber& number,
-									 const std::string& name,
-									 TestResult::EOutcome (*runFct)(Test& test),
-									 const TestEnvironment& environment)
-	: Test(number, name, environment), m_runFctVoid(0), m_runFctTest(runFct)
+                                     const std::string& name,
+                                     TestResult::EOutcome (*runFct)(Test& test),
+                                     const TestEnvironment& environment)
+    : Test(number, name, environment), m_runFctVoid(0), m_runFctTest(runFct)
 {
 }
 
@@ -72,18 +72,18 @@ FunctionBasedTest::~FunctionBasedTest() throw()
 
 TestResult::EOutcome FunctionBasedTest::doRun(TestObserver::ptr& observer)
 {
-	if (m_runFctTest)
-	{
-		return m_runFctTest(*this);
-	}
-	else if (m_runFctVoid)
-	{
-		return m_runFctVoid();
-	}
-	else
-	{
-		return TestResult::eFailed;
-	}
+    if (m_runFctTest)
+    {
+        return m_runFctTest(*this);
+    }
+    else if (m_runFctVoid)
+    {
+        return m_runFctVoid();
+    }
+    else
+    {
+        return TestResult::eFailed;
+    }
 }
 
 }
