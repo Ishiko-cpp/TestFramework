5ec4b2be26144d78b588ad0acfc106bf38778079
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2019-03-01 23:39:37 +0000

    Test updates

--- Tests/Core/Source/FunctionBasedTestTests/FunctionBasedTestTests.cpp
+++ Tests/Core/Source/FunctionBasedTestTests/FunctionBasedTestTests.cpp
@@ -1,166 +1,153 @@
 /*
-	Copyright (c) 2007-2015 Xavier Leclercq
+    Copyright (c) 2007-2019 Xavier Leclercq
 
-	Permission is hereby granted, free of charge, to any person obtaining a
-	copy of this software and associated documentation files (the "Software"),
-	to deal in the Software without restriction, including without limitation
-	the rights to use, copy, modify, merge, publish, distribute, sublicense,
-	and/or sell copies of the Software, and to permit persons to whom the
-	Software is furnished to do so, subject to the following conditions:
+    Permission is hereby granted, free of charge, to any person obtaining a
+    copy of this software and associated documentation files (the "Software"),
+    to deal in the Software without restriction, including without limitation
+    the rights to use, copy, modify, merge, publish, distribute, sublicense,
+    and/or sell copies of the Software, and to permit persons to whom the
+    Software is furnished to do so, subject to the following conditions:
 
-	The above copyright notice and this permission notice shall be included in
-	all copies or substantial portions of the Software.
+    The above copyright notice and this permission notice shall be included in
+    all copies or substantial portions of the Software.
 
-	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
-	THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
-	FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
-	IN THE SOFTWARE.
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+    THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+    FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+    IN THE SOFTWARE.
 */
 
 #include "FunctionBasedTestTests.h"
 
-void AddFunctionBasedTestTests(Ishiko::TestFramework::TestHarness& theTestHarness)
+using namespace Ishiko::TestFramework;
+
+void AddFunctionBasedTestTests(TestHarness& theTestHarness)
 {
-	Ishiko::TestFramework::TestSequence& functionBasedTestSequence =
-		theTestHarness.appendTestSequence("FunctionBasedTest tests");
+    TestSequence& testSequence = theTestHarness.appendTestSequence("FunctionBasedTest tests");
 
-	new Ishiko::TestFramework::HeapAllocationErrorsTest("Creation test 1",
-		FunctionBasedTestCreationTest1, functionBasedTestSequence);
-	new Ishiko::TestFramework::HeapAllocationErrorsTest("Creation test 2",
-		FunctionBasedTestCreationTest2, functionBasedTestSequence);
-
-	new Ishiko::TestFramework::HeapAllocationErrorsTest("run success test 1",
-		FunctionBasedTestRunSuccessTest1, functionBasedTestSequence);
-	new Ishiko::TestFramework::HeapAllocationErrorsTest("run success test 2",
-		FunctionBasedTestRunSuccessTest2, functionBasedTestSequence);
-
-	new Ishiko::TestFramework::HeapAllocationErrorsTest("run failure test 1",
-		FunctionBasedTestRunFailureTest1, functionBasedTestSequence);
-	new Ishiko::TestFramework::HeapAllocationErrorsTest("run failure test 2",
-		FunctionBasedTestRunFailureTest2, functionBasedTestSequence);
+    testSequence.append<HeapAllocationErrorsTest>("Creation test 1", FunctionBasedTestCreationTest1);
+    testSequence.append<HeapAllocationErrorsTest>("Creation test 2", FunctionBasedTestCreationTest2);
+    testSequence.append<HeapAllocationErrorsTest>("run success test 1", FunctionBasedTestRunSuccessTest1);
+    testSequence.append<HeapAllocationErrorsTest>("run success test 2", FunctionBasedTestRunSuccessTest2);
+    testSequence.append<HeapAllocationErrorsTest>("run failure test 1", FunctionBasedTestRunFailureTest1);
+    testSequence.append<HeapAllocationErrorsTest>("run failure test 2", FunctionBasedTestRunFailureTest2);
 }
 
-Ishiko::TestFramework::TestResult::EOutcome FunctionBasedTestCreationTest1Helper()
+TestResult::EOutcome FunctionBasedTestCreationTest1Helper()
 {
-	return Ishiko::TestFramework::TestResult::ePassed;
+    return TestResult::ePassed;
 }
 
-Ishiko::TestFramework::TestResult::EOutcome FunctionBasedTestCreationTest1()
+TestResult::EOutcome FunctionBasedTestCreationTest1()
 {
-	Ishiko::TestFramework::FunctionBasedTest test(Ishiko::TestFramework::TestNumber(), 
-		"FunctionBasedTestCreationTest1", FunctionBasedTestCreationTest1Helper);
-	return Ishiko::TestFramework::TestResult::ePassed;
+    FunctionBasedTest test(TestNumber(), "FunctionBasedTestCreationTest1", FunctionBasedTestCreationTest1Helper);
+    return TestResult::ePassed;
 }
 
-Ishiko::TestFramework::TestResult::EOutcome FunctionBasedTestCreationTest2Helper(Ishiko::TestFramework::Test& test)
+TestResult::EOutcome FunctionBasedTestCreationTest2Helper(Test& test)
 {
-	return Ishiko::TestFramework::TestResult::ePassed;
+    return TestResult::ePassed;
 }
 
-Ishiko::TestFramework::TestResult::EOutcome FunctionBasedTestCreationTest2()
+TestResult::EOutcome FunctionBasedTestCreationTest2()
 {
-	Ishiko::TestFramework::FunctionBasedTest test(Ishiko::TestFramework::TestNumber(),
-		"FunctionBasedTestCreationTest2", FunctionBasedTestCreationTest2Helper);
-	return Ishiko::TestFramework::TestResult::ePassed;
+    FunctionBasedTest test(TestNumber(), "FunctionBasedTestCreationTest2", FunctionBasedTestCreationTest2Helper);
+    return TestResult::ePassed;
 }
 
-Ishiko::TestFramework::TestResult::EOutcome FunctionBasedTestRunSuccessTest1Helper()
+TestResult::EOutcome FunctionBasedTestRunSuccessTest1Helper()
 {
-	return Ishiko::TestFramework::TestResult::ePassed;
+    return TestResult::ePassed;
 }
 
-Ishiko::TestFramework::TestResult::EOutcome FunctionBasedTestRunSuccessTest1()
+TestResult::EOutcome FunctionBasedTestRunSuccessTest1()
 {
-	Ishiko::TestFramework::FunctionBasedTest test(Ishiko::TestFramework::TestNumber(),
-		"FunctionBasedTestRunSuccessTest1", FunctionBasedTestRunSuccessTest1Helper);
-	test.run();
+    FunctionBasedTest test(TestNumber(), "FunctionBasedTestRunSuccessTest1", FunctionBasedTestRunSuccessTest1Helper);
+    test.run();
 
-	if (test.result().outcome() == Ishiko::TestFramework::TestResult::ePassed)
-	{
-		return Ishiko::TestFramework::TestResult::ePassed;
-	}
-	else
-	{
-		return Ishiko::TestFramework::TestResult::eFailed;
-	}
+    if (test.result().outcome() == TestResult::ePassed)
+    {
+        return TestResult::ePassed;
+    }
+    else
+    {
+        return TestResult::eFailed;
+    }
 }
 
-Ishiko::TestFramework::TestResult::EOutcome FunctionBasedTestRunSuccessTest2Helper(Ishiko::TestFramework::Test& test)
+TestResult::EOutcome FunctionBasedTestRunSuccessTest2Helper(Test& test)
 {
-	if (test.name() == "FunctionBasedTestRunSuccessTest2")
-	{
-		return Ishiko::TestFramework::TestResult::ePassed;
-	}
-	else
-	{
-		return Ishiko::TestFramework::TestResult::eFailed;
-	}
+    if (test.name() == "FunctionBasedTestRunSuccessTest2")
+    {
+        return TestResult::ePassed;
+    }
+    else
+    {
+        return TestResult::eFailed;
+    }
 }
 
-Ishiko::TestFramework::TestResult::EOutcome FunctionBasedTestRunSuccessTest2()
+TestResult::EOutcome FunctionBasedTestRunSuccessTest2()
 {
-	Ishiko::TestFramework::FunctionBasedTest test(Ishiko::TestFramework::TestNumber(),
-		"FunctionBasedTestRunSuccessTest2", FunctionBasedTestRunSuccessTest2Helper);
-	test.run();
+    FunctionBasedTest test(TestNumber(), "FunctionBasedTestRunSuccessTest2", FunctionBasedTestRunSuccessTest2Helper);
+    test.run();
 
-	if (test.result().outcome() == Ishiko::TestFramework::TestResult::ePassed)
-	{
-		return Ishiko::TestFramework::TestResult::ePassed;
-	}
-	else
-	{
-		return Ishiko::TestFramework::TestResult::eFailed;
-	}
+    if (test.result().outcome() == TestResult::ePassed)
+    {
+        return TestResult::ePassed;
+    }
+    else
+    {
+        return TestResult::eFailed;
+    }
 }
 
-Ishiko::TestFramework::TestResult::EOutcome FunctionBasedTestRunFailureTest1Helper()
+TestResult::EOutcome FunctionBasedTestRunFailureTest1Helper()
 {
-	return Ishiko::TestFramework::TestResult::eFailed;
+    return TestResult::eFailed;
 }
 
-Ishiko::TestFramework::TestResult::EOutcome FunctionBasedTestRunFailureTest1()
+TestResult::EOutcome FunctionBasedTestRunFailureTest1()
 {
-	Ishiko::TestFramework::FunctionBasedTest test(Ishiko::TestFramework::TestNumber(),
-		"FunctionBasedTestRunFailureTest1", FunctionBasedTestRunFailureTest1Helper);
-	test.run();
+    FunctionBasedTest test(TestNumber(), "FunctionBasedTestRunFailureTest1", FunctionBasedTestRunFailureTest1Helper);
+    test.run();
 
-	if (test.result().outcome() == Ishiko::TestFramework::TestResult::eFailed)
-	{
-		return Ishiko::TestFramework::TestResult::ePassed;
-	}
-	else
-	{
-		return Ishiko::TestFramework::TestResult::eFailed;
-	}
+    if (test.result().outcome() == TestResult::eFailed)
+    {
+        return TestResult::ePassed;
+    }
+    else
+    {
+        return TestResult::eFailed;
+    }
 }
 
-Ishiko::TestFramework::TestResult::EOutcome FunctionBasedTestRunFailureTest2Helper(Ishiko::TestFramework::Test& test)
+TestResult::EOutcome FunctionBasedTestRunFailureTest2Helper(Test& test)
 {
-	if (test.name() != "FunctionBasedTestRunFailureTest2")
-	{
-		return Ishiko::TestFramework::TestResult::ePassed;
-	}
-	else
-	{
-		return Ishiko::TestFramework::TestResult::eFailed;
-	}
+    if (test.name() != "FunctionBasedTestRunFailureTest2")
+    {
+        return TestResult::ePassed;
+    }
+    else
+    {
+        return TestResult::eFailed;
+    }
 }
 
-Ishiko::TestFramework::TestResult::EOutcome FunctionBasedTestRunFailureTest2()
+TestResult::EOutcome FunctionBasedTestRunFailureTest2()
 {
-	Ishiko::TestFramework::FunctionBasedTest test(Ishiko::TestFramework::TestNumber(),
-		"FunctionBasedTestRunFailureTest2", FunctionBasedTestRunFailureTest2Helper);
-	test.run();
+    FunctionBasedTest test(TestNumber(), "FunctionBasedTestRunFailureTest2", FunctionBasedTestRunFailureTest2Helper);
+    test.run();
 
-	if (test.result().outcome() == Ishiko::TestFramework::TestResult::eFailed)
-	{
-		return Ishiko::TestFramework::TestResult::ePassed;
-	}
-	else
-	{
-		return Ishiko::TestFramework::TestResult::eFailed;
-	}
+    if (test.result().outcome() == TestResult::eFailed)
+    {
+        return TestResult::ePassed;
+    }
+    else
+    {
+        return TestResult::eFailed;
+    }
 }
