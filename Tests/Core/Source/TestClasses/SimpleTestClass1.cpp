9a696cbc14ad26c77f104266e7ddf14bc7f4475a
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2019-03-01 00:27:28 +0000

    Test updates

--- Tests/Core/Source/TestClasses/SimpleTestClass1.cpp
+++ Tests/Core/Source/TestClasses/SimpleTestClass1.cpp
@@ -1,39 +1,44 @@
 /*
-	Copyright (c) 2007-2015 Xavier Leclercq
+    Copyright (c) 2007-2019 Xavier Leclercq
 
-	Permission is hereby granted, free of charge, to any person obtaining a
-	copy of this software and associated documentation files (the "Software"),
-	to deal in the Software without restriction, including without limitation
-	the rights to use, copy, modify, merge, publish, distribute, sublicense,
-	and/or sell copies of the Software, and to permit persons to whom the
-	Software is furnished to do so, subject to the following conditions:
+    Permission is hereby granted, free of charge, to any person obtaining a
+    copy of this software and associated documentation files (the "Software"),
+    to deal in the Software without restriction, including without limitation
+    the rights to use, copy, modify, merge, publish, distribute, sublicense,
+    and/or sell copies of the Software, and to permit persons to whom the
+    Software is furnished to do so, subject to the following conditions:
 
-	The above copyright notice and this permission notice shall be included in
-	all copies or substantial portions of the Software.
+    The above copyright notice and this permission notice shall be included in
+    all copies or substantial portions of the Software.
 
-	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
-	THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
-	FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
-	IN THE SOFTWARE.
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+    THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+    FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+    IN THE SOFTWARE.
 */
 
 #include "SimpleTestClass1.h"
 
-SimpleTestClass1::SimpleTestClass1(const Ishiko::TestFramework::TestNumber& number, 
-								   const std::string& name,
-								   Ishiko::TestFramework::TestResult::EOutcome outcome)
-	: Ishiko::TestFramework::Test(number, name), m_outcome(outcome)
+SimpleTestClass1::SimpleTestClass1(const Ishiko::TestFramework::TestNumber& number, const std::string& name,
+    Ishiko::TestFramework::TestResult::EOutcome outcome)
+    : Ishiko::TestFramework::Test(number, name), m_outcome(outcome)
 {
 }
 
-SimpleTestClass1::~SimpleTestClass1() throw()
+SimpleTestClass1::SimpleTestClass1(const Ishiko::TestFramework::TestNumber& number, const std::string& name,
+    Ishiko::TestFramework::TestResult::EOutcome outcome, const Ishiko::TestFramework::TestEnvironment& environment)
+    : Ishiko::TestFramework::Test(number, name, environment), m_outcome(outcome)
+{
+}
+
+SimpleTestClass1::~SimpleTestClass1() noexcept
 {
 }
 
 Ishiko::TestFramework::TestResult::EOutcome SimpleTestClass1::doRun(Ishiko::TestFramework::TestObserver::ptr& observer)
 {
-	return m_outcome;
+    return m_outcome;
 }
