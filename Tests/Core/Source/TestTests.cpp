fe290d71b6d889ec9657235b5150f29ff45869ff
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2021-05-02 17:00:16 +0100

    Add test

--- Tests/Core/Source/TestTests.cpp
+++ Tests/Core/Source/TestTests.cpp
@@ -1,23 +1,7 @@
 /*
-    Copyright (c) 2007-2019 Xavier Leclercq
-
-    Permission is hereby granted, free of charge, to any person obtaining a
-    copy of this software and associated documentation files (the "Software"),
-    to deal in the Software without restriction, including without limitation
-    the rights to use, copy, modify, merge, publish, distribute, sublicense,
-    and/or sell copies of the Software, and to permit persons to whom the
-    Software is furnished to do so, subject to the following conditions:
-
-    The above copyright notice and this permission notice shall be included in
-    all copies or substantial portions of the Software.
-
-    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
-    THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
-    FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
-    IN THE SOFTWARE.
+    Copyright (c) 2007-2021 Xavier Leclercq
+    Released under the MIT License
+    See https://github.com/Ishiko-cpp/TestFramework/blob/master/LICENSE.txt
 */
 
 #include "TestTests.h"
@@ -42,6 +26,7 @@ TestTests::TestTests(const TestNumber& number, const TestEnvironment& environmen
     append<HeapAllocationErrorsTest>("run test 2", RunTest2);
     append<HeapAllocationErrorsTest>("run test 3", RunTest3);
     append<HeapAllocationErrorsTest>("run test 4", RunTest4);
+    append<HeapAllocationErrorsTest>("run test 5", RunTest5);
     append<HeapAllocationErrorsTest>("abort test 1", AbortTest1);
 }
 
@@ -197,6 +182,20 @@ void TestTests::RunTest4(Test& test)
     myTest.run();
 
     ISHTF_FAIL_IF_NEQ(myTest.result(), TestResult::eFailed);
+    ISHTF_PASS();
+}
+
+void TestRunTest5Helper(Test& test)
+{
+    throw std::runtime_error("TestRunTest5Helper threw exception");
+}
+
+void TestTests::RunTest5(Test& test)
+{
+    Test myTest(TestNumber(1), "TestRunTest5", TestRunTest5Helper);
+    myTest.run();
+
+    ISHTF_FAIL_IF_NEQ(myTest.result(), TestResult::eException);
     ISHTF_PASS();
 }
 
