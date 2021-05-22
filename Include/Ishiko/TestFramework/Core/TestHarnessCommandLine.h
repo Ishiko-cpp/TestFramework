cd4e4aef24c7434f8f4c5a224e42482c0b7efab9
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2021-05-22 23:27:48 +0100

    Add files

---
+++ Include/Ishiko/TestFramework/Core/TestHarnessCommandLine.h
@@ -0,0 +1,10 @@
+/*
+    Copyright (c) 2021 Xavier Leclercq
+    Released under the MIT License
+    See https://github.com/Ishiko-cpp/TestFramework/blob/master/LICENSE.txt
+*/
+
+#ifndef _ISHIKO_TESTFRAMEWORK_CORE_TESTHARNESSCOMMANDLINE_H_
+#define _ISHIKO_TESTFRAMEWORK_CORE_TESTHARNESSCOMMANDLINE_H_
+
+#endif
