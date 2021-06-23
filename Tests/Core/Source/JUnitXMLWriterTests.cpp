395aba1e8903641541cf06ba56553abcdc7c8a0f
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2021-06-23 22:21:47 +0100

    Rename JUnitXMLFile to JUnitXMLWriter (#48)
    * Update test makefiles
    
    * Updates
    
    * Remove files
    
    * Updates
    
    * Update build files
    
    * Update build files
    
    * Update CI
    
    * Update build files
    
    * Update CI
    
    * Updates
    
    * Updates
    
    * Update copyright notices
    
    * Rename files
    
    * Updates

---
+++ Tests/Core/Source/JUnitXMLWriterTests.cpp
@@ -0,0 +1,23 @@
+/*
+    Copyright (c) 2021 Xavier Leclercq
+    Released under the MIT License
+    See https://github.com/Ishiko-cpp/TestFramework/blob/master/LICENSE.txt
+*/
+
+#include "JUnitXMLWriterTests.h"
+#include "Ishiko/TestFramework/Core/JUnitXMLWriter.h"
+
+using namespace Ishiko::Tests;
+
+JUnitXMLWriterTests::JUnitXMLWriterTests(const TestNumber& number, const TestEnvironment& environment)
+    : TestSequence(number, "JUnitXMLWriter tests", environment)
+{
+    append<HeapAllocationErrorsTest>("Constructor test 1", ConstructorTest1);
+}
+
+void JUnitXMLWriterTests::ConstructorTest1(Test& test)
+{
+    JUnitXMLWriter junitXMLFile;
+
+    ISHTF_PASS();
+}
--- core/Source/TestHarnessCommandLine.cpp
+++
@@ -1,7 +0,0 @@
-/*
-    Copyright (c) 2021 Xavier Leclercq
-    Released under the MIT License
-    See https://github.com/Ishiko-cpp/TestFramework/blob/master/LICENSE.txt
-*/
-
-#include "TestHarnessCommandLine.h"
