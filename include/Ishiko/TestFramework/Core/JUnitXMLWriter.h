b179223610db65100d24b7a03ec1544eed190291
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2021-06-26 10:18:02 +0100

    Add files

---
+++ include/Ishiko/TestFramework/Core/JUnitXMLWriter.h
@@ -0,0 +1,22 @@
+/*
+    Copyright (c) 2021 Xavier Leclercq
+    Released under the MIT License
+    See https://github.com/Ishiko-cpp/TestFramework/blob/master/LICENSE.txt
+*/
+
+#ifndef _ISHIKO_TESTFRAMEWORK_CORE_JUNITXMLWRITER_H_
+#define _ISHIKO_TESTFRAMEWORK_CORE_JUNITXMLWRITER_H_
+
+namespace Ishiko
+{
+namespace Tests
+{
+
+class JUnitXMLWriter
+{
+};
+
+}
+}
+
+#endif
