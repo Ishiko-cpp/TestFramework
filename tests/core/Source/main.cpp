e0476104201805dc3f326e4de077e9f07ff5c1ef
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2021-06-26 13:28:34 +0100

    Update main.cpp

--- tests/core/Source/main.cpp
+++ tests/core/Source/main.cpp
@@ -17,7 +17,7 @@
 #include "TestSetupActionsTests/TestSetupActionsTests.h"
 #include "TestTeardownActionsTests/TestTeardownActionsTests.h"
 #include "TestHarnessTests.h"
-#include <Ishiko/Tests/core.h>
+#include <Ishiko/Tests/Core.h>
 #include <boost/filesystem/operations.hpp>
 
 using namespace Ishiko::Tests;
