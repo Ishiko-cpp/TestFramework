aff9b59c748c9b91e0d6ce7c5b6efac4cae9aa53
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2021-07-14 21:31:41 +0100

    Refactoring (#80)

--- include/Ishiko/Tests/Core/ConsoleApplicationTest.h
+++ include/Ishiko/Tests/Core/ConsoleApplicationTest.h
@@ -1,11 +1,11 @@
 /*
-    Copyright (c) 2016-2019 Xavier Leclercq
+    Copyright (c) 2016-2021 Xavier Leclercq
     Released under the MIT License
-    See https://github.com/Ishiko-cpp/TestFramework/blob/master/LICENSE.txt
+    See https://github.com/ishiko-cpp/tests/blob/main/LICENSE.txt
 */
 
-#ifndef _ISHIKO_TESTFRAMEWORK_CORE_CONSOLEAPPLICATIONTEST_H_
-#define _ISHIKO_TESTFRAMEWORK_CORE_CONSOLEAPPLICATIONTEST_H_
+#ifndef _ISHIKO_TESTS_CORE_CONSOLEAPPLICATIONTEST_H_
+#define _ISHIKO_TESTS_CORE_CONSOLEAPPLICATIONTEST_H_
 
 #include "Test.h"
 #include "FileComparisonTest.h"
