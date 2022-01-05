3dbc2d3aa94fb1e21711e27b3f3c25f3f808c335
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2022-01-05 20:08:39 +0000

    Update copyright dates

--- core/src/TestProgressObserver.cpp
+++ core/src/TestProgressObserver.cpp
@@ -1,5 +1,5 @@
 /*
-    Copyright (c) 2015-2021 Xavier Leclercq
+    Copyright (c) 2015-2022 Xavier Leclercq
     Released under the MIT License
     See https://github.com/ishiko-cpp/tests/blob/main/LICENSE.txt
 */
