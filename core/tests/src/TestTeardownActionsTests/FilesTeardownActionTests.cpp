23bc1d86280f528475335321325766b6c8dfa9fd
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2022-01-22 19:49:29 +0000

    Fix test

--- core/tests/src/TestTeardownActionsTests/FilesTeardownActionTests.cpp
+++ core/tests/src/TestTeardownActionsTests/FilesTeardownActionTests.cpp
@@ -30,7 +30,7 @@ void FilesTeardownActionTeardownTest1Helper(Test& test)
     DebugHeap::TrackingState tracking;
     tracking.disableTracking();
 
-    path filePath(test.context().getTestOutputDirectory("TestTeardownActionsTests/FilesTeardownActionTeardownTest1"));
+    path filePath(test.context().getTestOutputPath("TestTeardownActionsTests/FilesTeardownActionTeardownTest1"));
     std::shared_ptr<FilesTeardownAction> action = std::make_shared<FilesTeardownAction>();
     action->add(filePath);
     test.addTeardownAction(action);
