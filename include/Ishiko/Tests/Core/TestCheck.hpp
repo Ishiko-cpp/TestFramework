9f0538356c317f4ef6f9036dfc7a13a65c9b7811
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2022-04-04 15:26:34 +0100

    Add files

---
+++ include/Ishiko/Tests/Core/TestCheck.hpp
@@ -0,0 +1,10 @@
+/*
+    Copyright (c) 2022 Xavier Leclercq
+    Released under the MIT License
+    See https://github.com/ishiko-cpp/test-framework/blob/main/LICENSE.txt
+*/
+
+#ifndef _ISHIKO_CPP_TESTFRAMEWORK_CORE_TESTCHECK_HPP_
+#define _ISHIKO_CPP_TESTFRAMEWORK_CORE_TESTCHECK_HPP_
+
+#endif
