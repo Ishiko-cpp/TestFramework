4b84fe90df65abb606bd04b32edc8f2f23e5532b
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2022-04-29 16:26:22 +0100

    Updates

---
+++ include/Ishiko/TestFramework/Core/linkoptions.hpp
@@ -0,0 +1,16 @@
+/*
+    Copyright (c) 2022 Xavier Leclercq
+    Released under the MIT License
+    See https://github.com/ishiko-cpp/test-framework/blob/main/LICENSE.txt
+*/
+
+#ifndef _ISHIKO_CPP_TESTFRAMEWORK_CORE_LINKOPTIONS_HPP_
+#define _ISHIKO_CPP_TESTFRAMEWORK_CORE_LINKOPTIONS_HPP_
+
+#ifdef _DEBUG
+#pragma comment(lib, "IshikoTestFrameworkCore-d.lib")
+#else
+#pragma comment(lib, "IshikoTestFrameworkCore.lib")
+#endif
+
+#endif
