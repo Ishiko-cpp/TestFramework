3639586dd0c8eaf93895cd27f2eea98598d57786
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2022-06-09 16:56:42 +0100

    Implement JUnitXMLWriter (#137)

--- include/Ishiko/TestFramework/Core/JUnitXMLWriter.hpp
+++ include/Ishiko/TestFramework/Core/JUnitXMLWriter.hpp
@@ -7,11 +7,30 @@
 #ifndef _ISHIKO_CPP_TESTFRAMEWORK_CORE_JUNITXMLWRITER_HPP_
 #define _ISHIKO_CPP_TESTFRAMEWORK_CORE_JUNITXMLWRITER_HPP_
 
+#include <boost/filesystem.hpp>
+#include <Ishiko/Errors.hpp>
+#include <Ishiko/XML.hpp>
+#include <string>
+
 namespace Ishiko
 {
 
 class JUnitXMLWriter
 {
+public:
+    JUnitXMLWriter();
+    void create(const boost::filesystem::path& path, Error& error);
+    void close();
+
+    void writeTestSuitesStart();
+    void writeTestSuitesEnd();
+    void writeTestSuiteStart(size_t tests);
+    void writeTestSuiteEnd();
+    void writeTestCaseStart(const std::string& classname, const std::string& name);
+    void writeTestCaseEnd();
+
+private:
+    XMLWriter m_xmlWriter;
 };
 
 }
