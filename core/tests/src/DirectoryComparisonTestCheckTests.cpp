caec6ec4617102ad60ff3bbaf033800e3dd47e33
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2022-06-12 19:27:18 +0100

    Add test files

---
+++ core/tests/src/DirectoryComparisonTestCheckTests.cpp
@@ -0,0 +1,20 @@
+/*
+    Copyright (c) 2022 Xavier Leclercq
+    Released under the MIT License
+    See https://github.com/ishiko-cpp/test-framework/blob/main/LICENSE.txt
+*/
+
+#include "DirectoryComparisonTestCheckTests.hpp"
+
+using namespace Ishiko;
+
+DirectoryComparisonTestCheckTests::DirectoryComparisonTestCheckTests(const TestNumber& number,
+    const TestContext& context)
+    : TestSequence(number, "DirectoryComparisonTestCheck tests", context)
+{
+    append<HeapAllocationErrorsTest>("Constructor test 1", ConstructorTest1);
+}
+
+void DirectoryComparisonTestCheckTests::ConstructorTest1(Test& test)
+{
+}
