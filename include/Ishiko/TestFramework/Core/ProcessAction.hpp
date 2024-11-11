6fa72c3bf6d8d4e67440c67730f53561b9786dc7
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2024-11-11 17:37:46 +0000

    Update ProcessAction.hpp

--- include/Ishiko/TestFramework/Core/ProcessAction.hpp
+++ include/Ishiko/TestFramework/Core/ProcessAction.hpp
@@ -1,11 +1,8 @@
-/*
-    Copyright (c) 2016-2022 Xavier Leclercq
-    Released under the MIT License
-    See https://github.com/ishiko-cpp/test-framework/blob/main/LICENSE.txt
-*/
+// SPDX-FileCopyrightText: 2000-2024 Xavier Leclercq
+// SPDX-License-Identifier: BSL-1.0
 
-#ifndef _ISHIKO_CPP_TESTFRAMEWORK_CORE_PROCESSACTION_HPP_
-#define _ISHIKO_CPP_TESTFRAMEWORK_CORE_PROCESSACTION_HPP_
+#ifndef GUARD_ISHIKO_CPP_TESTFRAMEWORK_CORE_PROCESSACTION_HPP
+#define GUARD_ISHIKO_CPP_TESTFRAMEWORK_CORE_PROCESSACTION_HPP
 
 #include "TestSetupAction.hpp"
 #include "TestTeardownAction.hpp"
